dceee1d6aef2d63b77998e11ab66a36c03184510
saadali <saadali6991550@gmail.com>
2026-03-12 20:10:38 +0500

    3 find biggest nbr in array

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-function Name(params) {
-    const reverse= params.split('').reverse().join('')
-    return params === reverse
-}
-console.log(   Name('gfg'))
+const array= [1,4,5,7,43]
+const Ab= Math.max(...array)
+console.log(Ab)
