10e2941b400563e295418894d095d3080680c8f4
saadali <saadali6991550@gmail.com>
2026-03-13 20:36:44 +0500

    18 nested function

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
-function Factorial(n){
-    if(n==0 ||n==1){
-        return n
-    }
-    return n *Factorial (n-1)
+const numb1=2;
+const numb2=4;
 
+function Saad(){
+const name='nirvan'
+    function Add(){
+        return `${name} scored ${numb1+numb2}`
+    }
+   return Add()
 }
- console.log(Factorial(5))
+console.log(Saad())
