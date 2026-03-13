3bf6db3b52b892f01baf413c22a75f010fcf91f8
saadali <saadali6991550@gmail.com>
2026-03-13 20:32:10 +0500

    17 find factorial

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-function Name(num){
-    if(num<=1) return false
-    for(let i=2;i<num;i++){
-      {
-        if(num %i=== 0) return false
-      }  
-      return true
+
+function Factorial(n){
+    if(n==0 ||n==1){
+        return n
     }
-    
+    return n *Factorial (n-1)
+
 }
-console.log(Name(1))
+ console.log(Factorial(5))
