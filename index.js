5ccf0a79940fc022064751d398f1f5eca5f8ffea
saadali <saadali6991550@gmail.com>
2026-03-17 21:36:28 +0500

    19 closure

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-
-const numb1=2;
-const numb2=4;
-
-function Saad(){
-const name='nirvan'
-    function Add(){
-        return `${name} scored ${numb1+numb2}`
+function X(){
+    let a= 8;
+    function Y(){
+        console.log(a++)
     }
-   return Add()
+     return Y
 }
-console.log(Saad())
+ const Z=  X()
+ Z()
+ Z()
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+// const numb1=2;
+// const numb2=4;
+
+// function Saad(){
+// const name='nirvan'
+//     function Add(){
+//         return `${name} scored ${numb1+numb2}`
+// //     }
+// //    return Add()
+// // }
+// // console.log(Saad())
+// // const arr = new Array(5);
+
+// // console.log(arr.length);
+// // console.log('saad')
+// // console.log(2+ '3')
+// const array=[1,2,3,4,5]
+// const b= array.splice(2,0,7,8,0)
+// console.log(array)
