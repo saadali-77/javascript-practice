85d6d3f98d958bd9b0d8bc66de72054d138d3d80
saadali <saadali6991550@gmail.com>
2026-03-27 22:13:03 +0500

    24 this keyword

--- index.js
+++ index.js
@@ -1,70 +1,35 @@
-
-const Git= 'https://github.com/users/saadali-77'
- const User= fetch(Git)
- console.log(User)
- User.then((data)=>{
-    console.log(data)
- })
-// 1 promiseall
-// run fo all give output collectively and find first error and return
-// 2 promisesettled
-// run for all and give error and response in array at the end
-// 3 promiserace
-// in it it will give which come first and return
-// 4 promiseany
-// it just need atleast one response to succed
-
-
-
-
-
-
-// var a=5
-// function New(){
-//     console.log(a)
-//     var a=10;
-
+// this keyword
+// // below it will point to window object
+// console.log(this)
+// //'use strict'
+// // inside strict mode it will print undefinde
+// function Nomad(){
+//    // it will point to global object
+//    console.log(this)
 // }
-// const NB=New()
-// console.log(NB)
+// Nomad()
+// // this call print undefine
 
+// window.Nomad()
+// // it will point 
+console.log("running...") 
+// out to global object
 
+const obj1= {
+   a:1,
+   New:function(){
+      
+      console.log(this.a)
+      // it will print 1
+   }
+}
+obj1.New()
+const obj2={
+   a:2
+}
+// below this will show or point to to whole obj 
 
-// const radius=[3,2,1,4]
+obj1.New.call(obj2)
+// call apply and bind bind are sharing methods
 
-// const output= radius.reduce((acc,curr)=>{
-//     if(curr>acc){
-//         acc= curr
-//     }
-//     return acc
-// })
-// console.log(output)
-
-
-
-
-
-
-
-
-
-
-// const numb1=2;
-// const numb2=4;
-
-// function Saad(){
-// const name='nirvan'
-//     function Add(){
-//         return `${name} scored ${numb1+numb2}`
-// //     }
-// //    return Add()
-// // }
-// // console.log(Saad())
-// // const arr = new Array(5);
-
-// // console.log(arr.length);
-// // console.log('saad')
-// // console.log(2+ '3')
-// const array=[1,2,3,4,5]
-// const b= array.splice(2,0,7,8,0)
-// console.log(array)
+// this inside dome element reference to html element
