8e8d2e49f2e30637c471a70759349f054db30a5f
saadali <saadali6991550@gmail.com>
2026-06-01 20:40:50 +0500

    23 factorial find logic

--- saad.js
+++ saad.js
@@ -29,16 +29,16 @@ console.log(b)
 
 
 
-// function factorial(n){
-//     let answer=1
-//     for(let i=1; i<=n; i++){
-//         answer *= i
-//     }
-//     return answer
+function factorial(n){
+    let answer=1
+    for(let i=1; i<=n; i++){
+        answer *= i
+    }
+    return answer
 
-// }
+}
 
-// console.log(factorial(5))
+console.log(factorial(5))
 
 
 
