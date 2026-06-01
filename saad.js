1f7e64c55822f82ef80157a9f30b55256ac809d2
saadali <saadali6991550@gmail.com>
2026-06-01 20:54:08 +0500

    26 intersection of two array

--- saad.js
+++ saad.js
@@ -1,8 +1,14 @@
 
-const a= [1,2,3]
-const b= [4,5,6]
-const c=a.concat(b)
-console.log(c)
+const a=[1,2,3,4]
+const b= [3,4,5,6]
+const c= new Set(a)
+const d= b.filter(x=>c.has(x))
+console.log(d)
+
+
+// const a= [1,2,3]
+// const c=a.concat(b)
+// console.log(c)
 
 
 
